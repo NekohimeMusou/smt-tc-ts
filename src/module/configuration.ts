69f453f80a571ac57e8e5a3b9e174c43c87b500d
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-04-21 15:22:10 -0400

    Consolidate stuff, remove space-filling comments

--- src/module/configuration.ts
+++ src/module/configuration.ts
@@ -1,34 +1,42 @@
-// The `export {}` is necessary because the file does not contain any top-level import/export statements.
-// `import Document = ...` does not count.
-//
-// Once you add an import or export statement, you can remove the `export {}`.
 export {};
 
-  import type { SmtItem } from "./documents/item/item";
-  import type { SkillData } from "./data-models/item/types/skill";
+import type { SmtItem } from "./documents/item/item";
+import type { SkillData } from "./data-models/item/types/skill";
 
-  // If you are writing a system then this file will be of use of you.
-  // Otherwise you can delete it.
-  declare module "fvtt-types/configuration" {
-    // foundry-vtt-types needs to know what documents you register with Foundry at runtime.
-    // You can configure every document class here.
-    //
-    // Tip 1: Make sure you import the document class and make sure the key is spelled right.
-    // Tip 2: If you want to make sure it's working, try writing `type TestConfiguration = ConfiguredDocumentClassForName<"Actor">;`
-    //        replacing `"Actor"` with the document type you're working with.
-    //        If hovering `TestConfiguration` says `typeof MyActor`, congratulations! It's working.
-    // Tip 2: If `MyActor` has generic parameters without defaults, you need to provide them here.
-    //        For example `typeof Actor<"npc" | "character">`. Make sure that the generic parameters are as wide as possible.
-    interface DocumentClassConfig {
-      Item: typeof SmtItem<ItemType>;
-    }
+/**
+ * CONFIG CONSTANTS
+ *
+ * These are normally defined in ./config/config.ts; they're moved here to slim down the example
+ * I tried simplifying it all the way to `type ItemType = "skill";` and it didn't seem to make a difference,
+ * so I stuck with (a pointless version of) the method I've been using all along in case it matters.
+ */
 
-    // foundry-vtt-types needs to know what data models you register with Foundry at runtime.
-    interface DataModelConfig {
-      Item: {
-        skill: typeof SkillData;
-      };
-    }
+declare global {
+  type ItemType = keyof typeof itemTypes;
+}
+
+const itemTypes = {
+  skill: "TYPES.Item.skill",
+};
+
+export const SMT = {
+  itemTypes,
+} as const;
+
+/**
+ * END CONFIG CONSTANTS
+ */
+
+declare module "fvtt-types/configuration" {
+  interface DocumentClassConfig {
+    Item: typeof SmtItem<ItemType>;
   }
+
+  interface DataModelConfig {
+    Item: {
+      skill: typeof SkillData;
+    };
+  }
+}
 
 // type TestConfiguration = Document.ImplementationClassFor<"Actor">;
--- src/module/index.ts
+++ src/module/index.ts
@@ -1,12 +1,4 @@
-// While many common classes are in the global scope such as `Actor` or `Item`, it seems Foundry has begun moving away from this.
-// Notably the `DataModel`, `DataField`, `ApplicationV2`, and other similar class is only accessible through some path.
-//
-// `import =` is an obscure syntax from the CommonJS era.
-// The pivotal part about it here is that it's effectively equivalent to:
-// `var DataModel = foundry.abstract.DataModel`.
-// But it also lets you write things like `DataModel.Any` that refers to the `DataModel` namespace.
-
-import { SMT } from "./config/config";
+import { SMT } from "./configuration";
 import { ITEMMODELS } from "./data-models/item/item-data-models";
 import { SmtItem } from "./documents/item/item";
 
@@ -21,10 +13,12 @@ declare global {
     SMT: typeof SMT;
   }
 }
+
 Hooks.on("ready", () => {
   console.log("SMT: TC | Initializing SMT: Tokyo Conception game system");
 
   CONFIG.SMT = SMT;
+  game.smt = { SmtItem };
 
   registerDataModels();
   registerDocumentClasses();
